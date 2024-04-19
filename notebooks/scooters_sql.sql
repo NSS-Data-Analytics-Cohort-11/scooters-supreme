7d28d3cb7131646c028252a8494480ad007ea398
eemchadwick <eem.chadwick@gmail.com>
2024-04-18 21:35:03 -0500

    end of Apr 18

--- notebooks/scooters_sql.sql
+++ notebooks/scooters_sql.sql
@@ -25,14 +25,39 @@ SELECT COUNT(triprecordnum)
 from trips;
 
 --finding null values: 
-SELECT *
+SELECT chargelevel, companyname
 FROM scooters
 WHERE chargelevel is null
--- or latitude is null
--- or longitude is null
--- or sumdid is null
--- or sumdtype is null
--- or chargelevel is null
--- or sumdgroup is null
--- or costpermin is null
--- or companyname is null;
+GROUP BY companyname, chargelevel;
+
+SELECT MIN(pubdatetime), MAX(pubdatetime)
+FROM scooters;
+
+SELECT DISTINCT(sumdgroup)
+FROM scooters
+
+SELECT COUNT(sumdgroup)
+FROM scooters
+WHERE sumdgroup = 'bicycle';
+--26529 bicycles
+
+SELECT COUNT(sumdgroup)
+FROM scooters
+WHERE sumdgroup = 'scooter';
+--59,671,463 scooters
+
+SELECT COUNT(sumdgroup)
+FROM scooters
+WHERE sumdgroup = 'Scooter';
+--13,716,051 Scooters
+
+SELECT MIN(latitude) AS min_lat, MAX(latitude) as max_lat, MIN(longitude) as min_long, MAX(longitude) as max_long
+FROM scooters
+LIMIT 10; 
+
+SELECT companyname
+FROM scooters
+WHERE latitude = 0.000000
+OR longitude = 0.000000
+GROUP BY  companyname
+--all 0 values for lat and long are from Gotcha scooters
